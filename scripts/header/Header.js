7b9825bc2a9344dbe4a95a52ffa2de25ba316246
durrjp <durrjp1@gmail.com>
2020-03-05 16:06:57 -0600

    more styles and new header

--- scripts/header/Header.js
+++ scripts/header/Header.js
@@ -1,5 +1,5 @@
 export const UseHeader = () => {
     return `
-    <header><img src="https://ov12-engine.flamingtext.com/netfu/tmp28004/coollogo_com-269821229.png"></header>
+    <header><img height="250px" src="dadpic.PNG"></header>
     `
 }
